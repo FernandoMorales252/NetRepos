c697d059a37f74cc250ebfee66d68bc9530beaf9
LUIS FERNANDO MORALES PEREZ <Fernandomoe09@outlook.com>
2025-09-16 15:22:05 -0600

    Controladores y pruebas iniciales

---
+++ AuthApi/Controllers/CategoriaController.cs
@@ -0,0 +1,52 @@
+﻿using AuthApi.DTOs.CategoriaDTOs;
+using AuthApi.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize] // Solo usuarios autenticados
+    public class CategoriaController : Controller
+    {
+        private readonly ICategoriaService _service;
+
+        public CategoriaController(ICategoriaService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+            => Ok(await _service.GetAllAsync());
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var item = await _service.GetByIdAsync(id);
+            return item is null ? NotFound() : Ok(item);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CategoriaCreateDTO dto)
+        {
+            var created = await _service.CreateAsync(dto);
+            return CreatedAtAction(nameof(GetById), new { id = created.Id }, created);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Update(int id, [FromBody] CategoriaUpdateDTO dto)
+        {
+            var ok = await _service.UpdateAsync(id, dto);
+            return ok ? NoContent() : NotFound();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var ok = await _service.DeleteAsync(id);
+            return ok ? NoContent() : NotFound();
+        }
+    }
+}
---
+++ AuthApi/Controllers/UsuarioController.cs
@@ -0,0 +1,26 @@
+﻿using AuthApi.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsuarioController : Controller
+    {
+        private readonly IUsuarioRepository usuarioRepository;
+
+        public UsuarioController(IUsuarioRepository pUsuarioRepository)
+        {
+            usuarioRepository = pUsuarioRepository;
+        }
+
+        [HttpGet("usuarios")]
+        [Authorize] // Solo usuarios autenticados
+        public async Task<IActionResult> GetUsuarios()
+        {
+            var usuarios = await usuarioRepository.GetAllUsuariosAsync();
+            return Ok(usuarios);
+        }
+    }
+}
