716ca94cbb00665bab6f184791b39c0317f2a768
LUIS FERNANDO MORALES PEREZ <Fernandomoe09@outlook.com>
2025-10-15 15:07:29 -0600

    Integracion de pruebas Unit para usuario

---
+++ AuthApi.TestXUnit/UsuarioRepositoryTest.cs
@@ -0,0 +1,84 @@
+﻿using AuthApi.Entidades;
+using AuthApi.Repositorios;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthApi.TestXUnit
+{
+    public class UsuarioRepositoryTest
+    {
+        private AppDbContext GetInMemoryDbContext()
+        {
+
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: $"TestDb_{System.Guid.NewGuid()}").Options;
+
+            var context = new AppDbContext(options);
+
+            context.Roles.Add(
+                new Entidades.Rol 
+                { Id = 1, 
+                  Nombre = "Admin" 
+                });
+
+            context.Usuarios.Add(
+                new Entidades.Usuarios 
+                { Id = 2, 
+                  Nombre = "Fernando" ,
+                  Email = "Fernando@123", 
+                  Password ="123" ,
+                  RolId= 1
+                });
+
+            context.SaveChanges();
+            return context;
+
+ 
+        }
+
+        [Fact]
+        public async Task GetByEmaiLAsync_ReturnUser()
+        {
+            //Arrange
+            var context = GetInMemoryDbContext();
+            var repo = new UsuarioRepository(context);
+
+            //Act
+            var usuario = await repo.GetByEmailAsync("Fernando@123");
+
+            //Assert
+            Assert.NotNull(usuario);
+            Assert.Equal("Fernando",  usuario.Nombre);
+            Assert.Equal("Admin", usuario.Rol.Nombre);
+        }
+
+        [Fact]
+        public async Task AddAsync_AddUser()
+        {
+            //Arrange
+            var context = GetInMemoryDbContext();
+            var repo = new UsuarioRepository(context);
+
+            var muevousuario = new Usuarios()
+            {
+                Nombre = "Maria",
+                Email = "Maria@gmail.com",
+                Password = "123",
+                RolId = 1
+
+            };
+
+            //Act
+            await repo.AddAsync(muevousuario);
+            //Assert
+            var usuarioGuardado = await context.Usuarios.FirstOrDefaultAsync(u => u.Email == "Maria@gmail.com");
+            Assert.NotNull(usuarioGuardado);
+            Assert.Equal("Maria", usuarioGuardado.Nombre);
+        }
+
+    }
+}
